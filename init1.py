d152813698d6b5e8a03138d3828a606c47d38070
BuildTools <unconfigured@null.spigotmc.org>
2022-04-13 15:29:09 -0400

    divide register and log in pages

--- init1.py
+++ init1.py
@@ -1,11 +1,11 @@
-#Import Flask Library
+# Import Flask Library
 from flask import Flask, render_template, request, session, url_for, redirect
 import pymysql.cursors
 
-#Initialize the app from Flask
+# Initialize the app from Flask
 app = Flask(__name__)
 
-#Configure MySQL
+# Configure MySQL
 conn = pymysql.connect(host='localhost',
                        user='root',
                        password='',
@@ -13,78 +13,111 @@ conn = pymysql.connect(host='localhost',
                        charset='utf8mb4',
                        cursorclass=pymysql.cursors.DictCursor)
 
-#Define a route to hello function
-@app.route('/')
-def hello():
-	return render_template('index.html')
 
-#Define route for login
+# Define a route to hello function
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+# Define route for login
 @app.route('/login')
 def login():
-	return render_template('login.html')
+    return render_template('login.html')
 
-#Define route for register
+
+# Define route for register
 @app.route('/register')
 def register():
-	return render_template('register.html')
+    return render_template('register.html')
 
-#Authenticates the login
+
+# Authenticates the login
 @app.route('/loginAuth', methods=['GET', 'POST'])
 def loginAuth():
-	#grabs information from the forms
-	username = request.form['username']
-	password = request.form['password']
+    # grabs information from the forms
+    username = request.form['username']
+    password = request.form['password']
 
-	#cursor used to send queries
-	cursor = conn.cursor()
-	#executes query
-	query = 'SELECT * FROM user WHERE username = %s and password = %s'
-	cursor.execute(query, (username, password))
-	#stores the results in a variable
-	data = cursor.fetchone()
-	#use fetchall() if you are expecting more than 1 data row
-	cursor.close()
-	error = None
-	if(data):
-		#creates a session for the the user
-		#session is a built in
-		session['username'] = username
-		return redirect(url_for('home'))
-	else:
-		#returns an error message to the templates page
-		error = 'Invalid login or username'
-		return render_template('login.html', error=error)
+    # cursor used to send queries
+    cursor = conn.cursor()
+    # executes query
+    query = 'SELECT * FROM user WHERE username = %s and password = %s'
+    cursor.execute(query, (username, password))
+    # stores the results in a variable
+    data = cursor.fetchone()
+    # use fetchall() if you are expecting more than 1 data row
+    cursor.close()
+    error = None
+    if (data):
+        # creates a session for the the user
+        # session is a built in
+        session['username'] = username
+        return redirect(url_for('home'))
+    else:
+        # returns an error message to the templates page
+        error = 'Invalid login or username'
+        return render_template('login.html', error=error)
 
-#Authenticates the register
+
+# Authenticates the register
 @app.route('/registerAuth', methods=['GET', 'POST'])
-def registerAuth():
-	#grabs information from the forms
-	username = request.form['username']
-	password = request.form['password']
+def registerType():
+    # grabs information from the forms
+    user_type = request.form['usertype']
 
-	#cursor used to send queries
-	cursor = conn.cursor()
-	#executes query
-	query = 'SELECT * FROM user WHERE username = %s'
-	cursor.execute(query, (username))
-	#stores the results in a variable
-	data = cursor.fetchone()
-	#use fetchall() if you are expecting more than 1 data row
-	error = None
-	if(data):
-		#If the previous query returns data, then user exists
-		error = "This user already exists"
-		return render_template('register.html', error = error)
-	else:
-		ins = 'INSERT INTO user VALUES(%s, %s)'
-		cursor.execute(ins, (username, password))
-		conn.commit()
-		cursor.close()
-		return render_template('index.html')
+    if user_type == "customer":
+        return render_template('register_customer.html')
+    else:
+        return render_template('register_staff.html')
+
+
+@app.route('/registerCustomer', methods=['GET', 'POST'])
+def registerAuthCustomer():
+    # grabs information from the forms
+    name = request.form['name']
+    email = request.form['email']
+    password = request.form['password']
+    date_of_birth = request.form['dateofbirth']
+    building_num = request.form['buildingnum']
+    street = request.form['street']
+    city = request.form['city']
+    state = request.form['state']
+    passport_num = request.form['passportnum']
+    passport_exp = request.form['passportexp']
+    passport_country = request.form['passportcountry']
+
+    # cursor used to send queries
+    cursor = conn.cursor()
+
+    # executes query
+    query = 'SELECT * FROM customer WHERE email = %s'
+    cursor.execute(query, (email))
+
+    # stores the results in a variable
+    data = cursor.fetchone()
+
+    # use fetchall() if you are expecting more than 1 data row
+    error = None
+    if data:
+        # If the previous query returns data, then user exists
+        error = "This user already exists"
+        return render_template('register.html', error=error)
+    else:
+        ins = '''INSERT INTO customer (email, name, password, 
+                    building_num, street, city, state, passport_num, 
+                    passport_exp, passport_country, date_of_birth) 
+                    VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'''
+        cursor.execute(ins, (email, name, password, building_num, street, city,
+                             state, passport_num, passport_exp, passport_country, date_of_birth))
+        conn.commit()
+        cursor.close()
+        session['username'] = name
+        return render_template('home.html')
+
 
 @app.route('/home')
 def home():
-    
     username = session['username']
     cursor = conn.cursor();
     return render_template('home.html', username=username)
@@ -92,12 +125,13 @@ def home():
 
 @app.route('/logout')
 def logout():
-	session.pop('username')
-	return redirect('/')
-		
+    session.pop('username')
+    return redirect('/')
+
+
 app.secret_key = 'some key that you will never guess'
-#Run the app on localhost port 5000
-#debug = True -> you don't have to restart flask
-#for changes to go through, TURN OFF FOR PRODUCTION
+# Run the app on localhost port 5000
+# debug = True -> you don't have to restart flask
+# for changes to go through, TURN OFF FOR PRODUCTION
 if __name__ == "__main__":
-	app.run('127.0.0.1', 5000, debug = True)
+    app.run('127.0.0.1', 5000, debug=True)

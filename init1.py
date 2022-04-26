dbc3c2413296bafced2174f4becc041a5a59b44d
biancamacias <bimacias14@gmail.com>
2022-04-25 20:27:56 -0400

    adding functionality to index page for searching future flights and flight status

--- init1.py
+++ init1.py
@@ -144,9 +144,9 @@ def registerAuthCustomer():
         error = "This user already exists"
         return render_template('register.html', error=error)
     else:
-        ins = '''INSERT INTO customer (email, name, password, 
-                    building_num, street, city, state, passport_num, 
-                    passport_exp, passport_country, date_of_birth) 
+        ins = '''INSERT INTO customer (email, name, password,
+                    building_num, street, city, state, passport_num,
+                    passport_exp, passport_country, date_of_birth)
                     VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'''
         cursor.execute(ins, (email, name, password, building_num, street, city,
                              state, passport_num, passport_exp, passport_country, date_of_birth))
@@ -183,7 +183,7 @@ def registerAuthStaff():
         error = "This user already exists"
         return render_template('register.html', error=error)
     else:
-        ins = '''INSERT INTO airline_staff (username, password, first_name, last_name, date_of_birth, airline_name) 
+        ins = '''INSERT INTO airline_staff (username, password, first_name, last_name, date_of_birth, airline_name)
                     VALUES(%s, %s, %s, %s, %s, %s)'''
         cursor.execute(ins, (username, password, first_name, last_name, date_of_birth, airline_name))
         conn.commit()
@@ -231,7 +231,7 @@ def create_confirmation():
         return render_template('create_flight.html', error=error)
     else:
         query = '''INSERT INTO flight (flight_num, departure_date, departure_time, departure_airport, arrival_date,
-                               arrival_time, arrival_airport, base_price, airplane_id, airline_name, flight_status) 
+                               arrival_time, arrival_airport, base_price, airplane_id, airline_name, flight_status)
                                VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'''
         cursor.execute(query, (flight_num, departure_date, departure_time, departure_airport, arrival_date,
                                arrival_time, arrival_airport, base_price, airplane_id, airline_name, flight_status))
@@ -247,11 +247,51 @@ def home():
 
     return render_template('home.html', username=username)
 
-
 @app.route('/logout')
 def logout():
     session.pop('username')
     return redirect('/')
+
+@app.route('/searchFutureFlights', methods=['GET', 'POST'])
+def searchFutureFlights():
+    source_airport = request.form['sourceairport']
+    destination_airport = request.form['destinationairport']
+    departure_date = request.form['departuredate']
+
+    cursor = conn.cursor()
+
+    query = "SELECT * FROM flight WHERE departure_airport = %s AND arrival_airport = %s AND departure_date = %s"
+
+    cursor.execute(query, (source_airport, destination_airport, departure_date))
+
+    data = cursor.fetchall()
+
+    if data:
+        return render_template("view_future_flights.html", data=data)
+    else:
+        error = "No flights found"
+        return render_template("index.html", error=error)
+
+@app.route('/searchActiveFlights', methods=['GET','POST'])
+def searchActiveFlights():
+    airline_name = request.form['airlinename']
+    flight_num = requst.form['flightnum']
+    departure_airport = request.form['departureairport']
+    arrival_airport = request.form['arrivalairport']
+
+    cursor = conn.cursor()
+
+    query = "SELECT * FROM flight WHERE airline_name = %s AND flight_num = %s AND departure_airport = %s AND arrival_airport = %s"
+
+    cursor.execute(query, (airline_name, flight_num, departure_airport, arrival_airport))
+
+    data = cursor.fetchall()
+
+    if data:
+        return render_template("view_flight_status.html", data=data)
+    else:
+        error = "Flight not found"
+        return render_template("index.html", data=data)
 
 
 app.secret_key = 'some key that you will never guess'
